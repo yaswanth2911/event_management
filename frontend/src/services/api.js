5d12171832a0e79a0637a19839d418baaf0c9104
yaswanth <yaswanthgunakala29@gmail.com>
2026-07-11 00:00:33 +0530

    refactor: point axios gateway instance to live production backend server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://event-management-i1tc.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
